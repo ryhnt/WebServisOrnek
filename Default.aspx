a2eb1a232153d81522ec1c5e5f3983646a3e9b0e
Reyhan <reyhan.tasdemir@hotmail.com>
2022-04-06 23:52:21 +0300

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebServisOrnek.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div style="margin-top:100px;margin-left:200px">
+            <h1>Öğrenci Kayıt</h1>
+            <table>
+                <tr>
+                    <th>
+                        <asp:Label Text="Ad:" runat="server" />
+                    </th>
+                    <th>
+                        <asp:TextBox id="txtad" runat="server" />
+                    </th>
+                
+                  </tr>
+                <tr>
+                    <th>
+                        <asp:Label Text="Soyad:" runat="server" />
+                    </th>
+                    <th>
+                        <asp:TextBox id="txtsoyad" runat="server" />
+                    </th>
+                </tr>
+                 <tr>
+                    <th>
+                        <asp:Label Text="TC:" runat="server" />
+                    </th>
+                    <th>
+                        <asp:TextBox id="txttc" runat="server" Maxlength="11"/>
+                    </th>
+                
+                  </tr>
+                <tr>
+                    <th>
+                        <asp:Label Text="Doğum Yılı:" runat="server" />
+                    </th>
+                    <th>
+                        <asp:TextBox id="txtdogum" runat="server" />
+                    </th>
+                </tr>
+                  <tr>
+                    <th>
+                        <asp:Label Text="Şehir:" runat="server" />
+                    </th>
+                    <th>
+                        <asp:DropDownList ID="drop" runat="server"  AutoPostBack="true">
+                            
+                        </asp:DropDownList>
+                    </th>
+               
+                </tr>
+                <tr>
+                    <th>
+
+                    </th>
+                    <th>
+                        <asp:Button ID="Kaydet" Text="Kaydet" runat="server" OnClick="Kaydet_Click" />
+                    </th>
+                    </tr>
+                
+            </table>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WebServisOrnek
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("server=LAPTOP-9U5K7JLD\\SQLEXPRESS;Database=Okul;Integrated Security=True");
+        SqlCommand komut;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                servissehir.WebService1SoapClient s = new servissehir.WebService1SoapClient();
+
+                DataSet dt = new DataSet();
+
+                dt = s.sehirgetir();
+
+
+
+
+                drop.DataTextField = dt.Tables[0].Columns["sehir"].ToString();
+                drop.DataValueField = dt.Tables[0].Columns["id"].ToString();
+                drop.DataSource = dt.Tables[0];
+                drop.DataBind();
+            }
+           
+        }
+
+        protected void Kaydet_Click(object sender, EventArgs e)
+        {
+            long tc = long.Parse(txttc.Text);
+            int yil = int.Parse(txtdogum.Text);
+
+            bool? durum;
+            try
+            {
+                using(tcServis.KPSPublicSoapClient tcs=new tcServis.KPSPublicSoapClient())
+                {
+                    durum = tcs.TCKimlikNoDogrula(tc, txtad.Text, txtsoyad.Text, yil);
+                }
+
+            }
+            catch 
+            {
+
+                durum = null;
+            }
+
+            if (durum==true)
+            {
+               
+                komut = new SqlCommand("INSERT INTO Ogrenciler (Ad, Soyad,Tc,DogumYil,Sehir) VALUES(@adi, @soyadi,@tc,@yil,@sehir)",con);
+                komut.Parameters.AddWithValue("@adi", txtad.Text);
+                komut.Parameters.AddWithValue("@soyadi", txtsoyad.Text);
+                komut.Parameters.AddWithValue("@tc", txttc.Text);
+                komut.Parameters.AddWithValue("@yil", txtdogum.Text);
+                komut.Parameters.AddWithValue("@sehir", drop.SelectedItem.Text.ToString());
+
+                con.Open();
+                komut.ExecuteNonQuery();
+                Response.Write("<script language=javascript>alert('Kayıt Eklendi.');</script>");
+            }
+            else
+            {
+                Response.Write("<script language=javascript>alert('Tc Kimlik Hatalı.');</script>");
+            }
+
+           
+        }
+    }
+}
